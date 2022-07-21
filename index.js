ec27e1252e63e08a264c8e8ea18d6c9e0f42d2b6
Sagar Subedi <sagar@example.com>
2022-07-20 22:37:08 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+dotenv.config();
+
+
+
+
+app.listen(8800, () => {
+    console.log('Server is running on port 8800');
+});
